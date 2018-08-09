0f91726fb9a0e50bf958444db2876371ae420eff
Arturo Blazquez <arturo.blazquez@autentia.com>
2018-08-09 11:28:20 +0200

    prompt bonito

--- .ipython/profile_default/startup/MinimalPrompt.py
+++ .ipython/profile_default/startup/MinimalPrompt.py
@@ -1,12 +1,24 @@
+# -*- coding: utf-8 -*-
+
 from IPython.terminal.prompts import Prompts, Token
 import os
 
 class MyPrompt(Prompts):
     def in_prompt_tokens(self, cli=None):
-        return [(Token.Prompt, '>')]
+        return [
+            (Token.PromptNum, str(self.shell.execution_count)),
+            (Token.Prompt, u'│ ')
+        ]
+
+    def continuation_prompt_tokens(self, cli=None, width=None):
+        if width is None:
+            width = self._width()
+        return [
+            (Token.Prompt, (' ' * (width - 2)) + u'│ ')
+        ]
 
     def out_prompt_tokens(self, cli=None):
-        return [(Token.Prompt, '')]
+        return [(Token.Prompt, '\n')]
 
 ip = get_ipython()
 ip.prompts = MyPrompt(ip)
